f60b5528d560ca63165eb133c22182f2a87152ff
Jamison <jlevitin@gmail.com>
2018-06-07 15:01:05 -0700

    Added helmet middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
-const { Router } = require('./router')
+const { Router } = require('./router');
+const helmet = require('helmet');
 require('../db/models/photos');
 
 const app = express();
-
+app.use(helmet());
 app.use(parser.urlencoded({extended:true}));
 app.use(parser.json());
 
