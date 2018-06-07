10c0e6b13bb61adc14b717ace79837bfcbeda612
Jamison <jlevitin@gmail.com>
2018-06-07 14:57:18 -0700

    To more accurately emulate zoom (proper frame overlay, no cursor offset), switched from my PhotoZoom.jsx component to react-image-magnify. Added basic popover onClick photo gallery functionality that will be disabled

--- client/src/components/Photos.jsx
+++ client/src/components/Photos.jsx
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import styled from 'styled-components'
-import PhotoZoom from './PhotoZoom.jsx'
-import Thumbnails from './Thumbnails.jsx'
+import styled from 'styled-components';
+import PhotoZoom from './PhotoZoom.jsx';
+import ThumbRender from './ThumbRender.jsx';
+import ReactImageMagnify from 'react-image-magnify';
+import { AltImages, ImagesMain, ImagesLeft, ZoomContainer, ImageContainer, Popover, ImageGalMain, ImageGalThumb} from './style.js'
 
-const AltImages = styled.div`
-    width: 40px;
-    margin-left: 0px;
-    float: left;
-    
-`
-const ImagesMain = styled.div`
-  position: relative;
-  overflow: auto;
-`
-const ImagesLeft = styled.div`
-  ${'' /* padding: 32px; */}
-`
+
 class Photos extends Component {
   constructor(props){
     super(props)
     this.state = {
       images: [],
       main: '',
-      selected: []
+      selected: [],
+      isSelected: true,
+      pop: false
     }
-    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
     axios.get('api/pictures/1')
@@ -37,23 +28,59 @@ class Photos extends Component {
       })
     })
   }
-  handleClick(e) {
+  handleClick = (e) => {
       this.setState({
       main: e.target.src,
-      selected: e.target
+      selected: e.target,
+      isSelected: false
+    })
+  }
+  gallery = (e) => {
+    console.log('click registered', e.target)
+    this.setState({
+      pop: !this.state.pop
     })
   }
   render() {
+    if(this.state.pop) {
+      return (
+      <Popover>
+        <ImageGalMain src={this.state.main}/>
+        <ImageGalThumb>
+      {this.state.images.map((img, index) => (
+        <ThumbRender img={img} key={index} id={index} handleClick={this.handleClick} selected={this.state.selected} isSelected={this.state.isSelected}/>
+      ))}
+      </ImageGalThumb>
+        </Popover>
+        )
+    }
     return ( 
       <ImagesMain>
     <ImagesLeft>
   <AltImages>
       {this.state.images.map((img, index) => (
-      <Thumbnails img={img} key={index} id={index} handleClick={this.handleClick} selected={this.state.selected}/>
+      <ThumbRender img={img} key={index} id={index} handleClick={this.handleClick} selected={this.state.selected} isSelected={this.state.isSelected}/>
       ))}
   </AltImages>
   </ImagesLeft>
-    <PhotoZoom main={this.state.main}/>
+    {/* <PhotoZoom main={this.state.main} images={this.state.images} onClick={this.gallery}/> */}
+    <ImageContainer onClick={this.gallery}>
+    <ReactImageMagnify {...{
+      smallImage: {
+        src: this.state.main,
+        width: 400,
+        height: 260
+      },
+      largeImage: {
+        src: this.state.main,
+        width: 1024,
+        height: 768
+      },
+      // enlargedImagePortalId: 'zoom',
+      shouldUsePositiveSpaceLens: true
+    }} />
+    <ZoomContainer id='zoom'></ZoomContainer>
+    </ImageContainer>
       </ImagesMain>
     )
   }
