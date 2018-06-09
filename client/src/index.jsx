83fe7584cf433c8184567bd4ef535d48774d7ecc
Jamison <jlevitin@gmail.com>
2018-06-08 20:29:42 -0700

    Moved modal here for Enzyme

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import App from './App.jsx'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import App from './App.jsx';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root')
 
 render(<App />, document.getElementById('root'));
