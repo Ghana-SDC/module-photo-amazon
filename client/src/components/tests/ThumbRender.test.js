3e4f96285740cdb9d56ea61202abd640fcb442bc
Jamison <jlevitin@gmail.com>
2018-06-11 21:37:56 -0700

    Writing tests for new thumbrender functionality

--- client/src/components/tests/ThumbRender.test.js
+++ client/src/components/tests/ThumbRender.test.js
@@ -23,5 +23,15 @@ describe('Thumbnail component', () => {
     wrapper.setProps({ id: 1 });
     expect(wrapper.find('.thumbs').exists()).toBe(true);
   })
+  // it('should handle changing thumbnails on the main photogallery', () => {
+  //   const wrapper = mount(<ThumbRender />)
+  //   wrapper.setProps({id: 1, showModal: false});
+  //   wrapper.find('.thumbs').simulate('click');
+  //   expect(wrapper.find('.thumbs').get(0).props.style).toHaveProperty('border', '.8px solid #e56f14')
+  // })
+  // it('should see css', () => {
+  //   wrapper.setProps({id: 0});
+  //   expect(wrapper.find('.thumb').get(0).props.style).toHaveProperty('border', '.8px solid black')
+  // })
 
 })
